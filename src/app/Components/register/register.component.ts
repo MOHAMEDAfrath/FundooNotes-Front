f0b3f1038b68b459404158d71635ee3e580d20d1
MOHAMEDAfrath <afrathismath@gmail.com>
2021-09-15 18:06:57 +0530

    [Afrath]ADDED:- Added show password and input validation

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators, FormGroup,AbstractControl,ValidatorFn, FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
+  RegisterForm!:FormGroup;
+  hide = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.RegisterForm = new FormGroup({
+      firstName: new FormControl('',[Validators.required, Validators.pattern('^[A-Z]{1}[a-z]{1,}$'),Validators.minLength(3)]),
+      lastName: new FormControl('',[Validators.required, Validators.pattern('^[A-Z]{1}[a-z]{2,}([\\s]{0,1}[A-Za-z]{1,})*$'),Validators.minLength(3)]),
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl('',[Validators.required,Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^.,:;!@#$%^&*_+=|(){}\/\\[?-]*[.,:;!@#$%^&*_+=|(){}\/\\[?-][^.,:;!@#$%^&*_+=|(){}\/\\[?-]*$).{8,}$')]),
+      confirm:new FormControl('',Validators.required)
+    })
+   
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
 import { LoginComponent } from './Components/Login/login.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { LoginComponent } from './Components/Login/login.component';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
